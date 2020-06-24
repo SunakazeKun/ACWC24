80922c4a223ecb2e0604727558a6bfebaac2d3e6
SunakazeKun <nintendo.rpgforum@gmail.com>
2020-06-24 13:56:58 +0200

    improved shizz
    improved shizz

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # ACWC24
 **ACWC24** creates distributable files for DLC items, patterns and letters in Animal Crossing City Folk (also known as *Let's Go to the City* in PAL regions). These files can be stored on a server from where it is sent to other people's games. Because the official Nintendo servers have been shut down in 2013, [RiiConnect24](https://rc24.xyz/) was founded, which provides a great service to *reconnect* your Wii to the internet and download DLC. Therefore, this tool was created to make the process of creating distributables less tedious.
 
@@ -5,19 +6,19 @@
 Here's a quick overview of *ACWC24*'s features:
  - Pack distributable items, patterns or both!
  - Easily manage delivered letters
- - Supports NTSC-U, PAL, NTSC-J and NTSC-K regions!
+ - Supports all versions and regions!
  - Separate distributables for target regions
  - Create valid U8 archives for distributables
  - Automatically encrypt output files
 
-The game also appears to support DLC NPCs or something like that, however, this needs to be investigated.
+The game also appears to support DLC NPCs, however, this needs to be investigated.
 
 # Requirements
 In order to encrypt WC24 files, the *pyaes* and *rsa* modules are required. You can install them using *pip*:
 
     pip install pyaes
     pip install rsa
-Also, the AES and PEM keys for ACCF are required in order to properly sign the data. We can't share those, unfortunately. You are on your own finding them. If you manage to obtain the keys, put them in *rvforestdl.aes.bin* and *rvforestdl.pem.bin*.
+Also, the AES and PEM keys for ACCF are required in order to properly sign the data. I can't share those, unfortunately. You are on your own finding them. If you manage to obtain the keys, put them in *rvforestdl.aes.bin* and *rvforestdl.pem.bin*.
 If you don't have those keys, the tool will skip encryption and create the U8 archive only.
 In order to add content, put the binary item files (which can be created with *ACDLC*) in the *items* folder. Patterns belong to the *designs* folder.
 
@@ -28,41 +29,40 @@ Each distributable has a folder inside the *src* directory. They all require a p
 	    "Regions": [ "E", "P", "J", "K" ],
 	    "Unk0": 1,
 	    "Unk4": 1,
-	    "Unk8": 16,
+	    "LetterId": XX,
 	    "UnkC": 0,
 	    "Unk10": 0,
 	    "ItemFile": "<item file name>",
 	    "DesignFile": "<pattern file name>",
-	    "NpcFile": "<npc file name>"
+	    "NpcFile": "<npc file name>",
+	    "Paper": "<stationery name>",
+	    "Letters": { // optional specified letters for each language
+		    "UsEnglish": {
+			    "Header": "Dear \n,",
+			    "Body": "here is your special item!",
+			    "Footer": "from Aurum",
+			    "Sender": "Aurum"
+		    }
+	    }
 	}
 
-The target region is used to add the required letter text for each localization (if available). If you don't want to create a package for separate regions, simply change the list to contain *All*. The unknown parameters still need to be documented.
-Letters for each language are stored in separate files:
+The target region is used to add the required letter text for each localization (if available). If you don't want to create a package for separate regions, simply change the list to contain the element *All*. The unknown parameters still need to be documented but can be the above values if you just want to distribute items.
+Letters for each language are stored in the *Letters* object above. The keys for the different languages are:
 
-| Language | File name | Region |
+| Language | Key | Region |
 |--|--|--|
-| English (NOA) | usEnglish.json | E |
-| French (NOA) | usFrench.json | E |
-| Spanish (NOA) | usSpanish.json | E |
-| English (NOE) | euEnglish.json | P |
-| French (NOE) | euFrench.json | P |
-| Spanish (NOE) | euSpanish.json | P |
-| German | euGerman.json | P |
-| Italian | euItalian.json | P |
-| Japanese | jpJapanese.json | J |
-| Korean | krKorean.json | K |
+| English (NOA) | UsEnglish | E |
+| French (NOA) | UsFrench | E |
+| Spanish (NOA) | UsSpanish | E |
+| English (NOE) | EuEnglish | P |
+| French (NOE) | EuFrench | P |
+| Spanish (NOE) | EuSpanish | P |
+| German | German | P |
+| Italian | Italian | P |
+| Japanese | Japanese | J |
+| Korean | Korean | K |
 
-It's a basic JSON object which looks like this:
-
-    {
-	    "Paper": "elegant",
-	    "Sender": "RiiConnect24",
-	    "Header": "Dear \n,",
-	    "Body": "Here is a DLC item from\nRiiConnect24. It's an\nRC24 carpet! It'll show\nvisitors that you enjoy\nusing our service!",
-	    "Footer": "RiiConnect24"
-	}
-
-It's important to note that a line should never contain more than 26 characters or the game will not display the letter at all! Make sure to put line breaks.
+It's important to note that a line should never contain more than ~26 characters or the game will not display the letter at all! Make sure to put in line breaks.
 
 You can pack the distributable using the command
 
--- acwc24.py
+++ acwc24.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import os
 
 from tools.bitconv import put_uint32
 from tools.bmg import Bmg, Message
@@ -17,23 +16,79 @@ PAPERS = ["butterfly", "airmail", "New_Year_s_cards", "lacy", "cloudy", "petal",
           "crater", "bejeweled", "geometric", "southwest", "night_sky", "chic", "goldfish", "Halloween", "lantern",
           "auction", "bulletin"]
 
+DEFAULT_HEADERS = {
+    "UsEnglish": "Dear \n,",
+    "UsFrench": "À \n,",
+    "UsSpanish": "¡Hola, \n!",
+    "EuEnglish": "Dear \n,",
+    "EuFrench": "À \n,",
+    "EuSpanish": "¡Hola, \n!",
+    "German": "Hallo \n,",
+    "Italian": "Ciao \n,",
+    "Japanese": "Dear \n,",  # placeholder
+    "Korean": "Dear \n,"  # placeholder
+}
+DEFAULT_BODIES = {
+    "UsEnglish": "Thank you for using\nRiiConnect24. Attached is\na present from us:\n{0}\nEnjoy!",
+    "UsFrench": "Merci d'utiliser\nRiiConnect24. Un cadeau\nde notre part est attaché\nà cette lettre:\n{0}\nProfitez-en bien!",
+    "UsSpanish": "Gracias por usar\nRiiConnect24. Te enviamos\nun regalo de nuestra parte:\n{0}\n¡Que lo disfrutes!",
+    "EuEnglish": "Thank you for using\nRiiConnect24. Attached is\na present from us:\n{0}\nEnjoy!",
+    "EuFrench": "Merci d'utiliser\nRiiConnect24. Un cadeau\nde notre part est attaché\nà cette lettre:\n{0}\nProfitez-en bien!",
+    "EuSpanish": "Gracias por usar\nRiiConnect24. Te enviamos\nun regalo de nuestra parte:\n{0}\n¡Que lo disfrutes!",
+    "German": "Vielen Dank, dass du\nRiiConnect24 nutzt. Anbei\nfindest du ein Geschenk\nvon uns:\n{0}\nViel Spaß!",
+    "Italian": "Grazie per aver usato\nRiiConnect24. Abbiamo\nallegato un regalo da\nparte nostra:\n{0}\nDivertitevi!",
+    "Japanese": "Thank you for using\nRiiConnect24. Attached is\na present from us:\n{0}\nEnjoy!",  # placeholder
+    "Korean": "Thank you for using\nRiiConnect24. Attached is\na present from us:\n{0}\nEnjoy!"  # placeholder
+}
 
-def create_letter(letterpath: str):
-    with open(letterpath, "r", encoding="utf8") as f:
-        data = json.load(f)
 
+def get_item_name(itemdata, off):
+    return itemdata[off:off+0x22].decode("utf-16-be").strip("\0")
+
+
+def get_item_names(itemdata):
+    ret = dict()
+    ret["Japanese"] = get_item_name(itemdata, 0x012)
+    ret["UsEnglish"] = get_item_name(itemdata, 0x034)
+    ret["UsSpanish"] = get_item_name(itemdata, 0x056)
+    ret["UsFrench"] = get_item_name(itemdata, 0x078)
+    ret["EuEnglish"] = get_item_name(itemdata, 0x09A)
+    ret["German"] = get_item_name(itemdata, 0x0BC)
+    ret["Italian"] = get_item_name(itemdata, 0x0DE)
+    ret["EuSpanish"] = get_item_name(itemdata, 0x100)
+    ret["EuFrench"] = get_item_name(itemdata, 0x122)
+    ret["Korean"] = get_item_name(itemdata, 0x144)
+    return ret
+
+
+def create_letter(dlc_info: str, locale: str, item_names: dict):
+    paper = dlc_info["Paper"] if "Paper" in dlc_info else PAPERS[0]
+    # Check if letter exists for specified locale
+    if "Letters" in dlc_info and locale in dlc_info["Letters"]:
+        data = dlc_info["Letters"][locale]
+    # Create a dummy letter from RiiConnect24 if the data cannot be found
+    else:
+        data = dict()
+        data["Header"] = DEFAULT_HEADERS[locale]
+        data["Body"] = DEFAULT_BODIES[locale].format(item_names[locale])
+        data["Footer"] = "RiiConnect24"
+        data["Sender"] = "RiiConnect24"
+        data["Paper"] = paper
+
+    # Map text attributes to text
     default_attributes = bytes.fromhex("00000000000000000000000000000001")
-    message_attributes = bytes.fromhex("00000002140000000000000000000000")
+    header_attributes = bytes.fromhex("00000002140000000000000000000000")
     text_mappings = [
         ("", default_attributes),
-        (data["Header"], message_attributes),
+        (data["Header"], header_attributes),
         (data["Body"], default_attributes),
         (data["Footer"], default_attributes),
         (data["Sender"], default_attributes),
-        ("{0}".format(PAPERS.index(data["Paper"]) + 400), default_attributes),
+        ("{0}".format(PAPERS.index(paper) + 400), default_attributes),
         ("", default_attributes)
     ]
 
+    # Create and store the actual BMG data
     bmg = Bmg()
 
     for mapping in text_mappings:
@@ -45,63 +100,72 @@ def create_letter(letterpath: str):
     return bmg.save()
 
 
-def create(dlcname: str, keep_decrypted: bool = False):
-    srcpath = "src/" + dlcname
+def create(dlc_name: str, keep_decrypted: bool = False):
+    dlc_info = json.loads(read_file("src/" + dlc_name + ".json"))
 
-    if not os.path.isdir(srcpath):
-        print("Can't find " + dlcname)
-        return
+    # Create info.bin
+    info_bin = bytearray(20)
+    put_uint32(info_bin, 0x00, dlc_info["Unk0"])
+    put_uint32(info_bin, 0x04, dlc_info["Unk4"])
+    put_uint32(info_bin, 0x08, dlc_info["LetterId"])
+    put_uint32(info_bin, 0x0C, dlc_info["UnkC"])
+    put_uint32(info_bin, 0x10, dlc_info["Unk10"])
 
-    dlcinfo = json.load(open(srcpath + "/info.json", "r"))
-    infobin = bytearray(20)
-    put_uint32(infobin, 0x00, dlcinfo["Unk0"])
-    put_uint32(infobin, 0x04, dlcinfo["Unk4"])
-    put_uint32(infobin, 0x08, dlcinfo["Unk8"])
-    put_uint32(infobin, 0x0C, dlcinfo["UnkC"])
-    put_uint32(infobin, 0x10, dlcinfo["Unk10"])
+    item_file_name = dlc_info["ItemFile"]
+    design_file_name = dlc_info["DesignFile"]
+    npc_file_name = dlc_info["NpcFile"]
 
-    for region in dlcinfo["Regions"]:
+    if item_file_name:
+        item_data = read_file("items/" + item_file_name)
+        itemnames = get_item_names(item_data)
+    else:
+        item_data = None
+        itemnames = None
+    design_data = read_file("designs/" + design_file_name) if design_file_name else None
+    npc_data = read_file("npcs/" + npc_file_name) if npc_file_name else None
+
+    # Create separate distributables for each target region
+    for region in dlc_info["Regions"]:
+        # Create basic archive
         archive = U8()
-        archive.add_file("info.bin", infobin)
+        archive.add_file("info.bin", info_bin)
 
-        itemfile = dlcinfo["ItemFile"]
-        designfile = dlcinfo["DesignFile"]
-        npcfile = dlcinfo["NpcFile"]
-
-        if itemfile:
-            archive.add_file("item.bin", read_file("items/" + itemfile))
+        # Add contents to archive
+        if item_data:
+            archive.add_file("item.bin", item_data)
 
             if region == "E" or region == "All":
-                archive.add_file("ltrue.bmg", create_letter(srcpath + "/usEnglish.json"))
-                archive.add_file("ltruf.bmg", create_letter(srcpath + "/usFrench.json"))
-                archive.add_file("ltrus.bmg", create_letter(srcpath + "/usSpanish.json"))
-            elif region == "P" or region == "All":
-                archive.add_file("ltree.bmg", create_letter(srcpath + "/euEnglish.json"))
-                archive.add_file("ltref.bmg", create_letter(srcpath + "/euFrench.json"))
-                archive.add_file("ltreg.bmg", create_letter(srcpath + "/euGerman.json"))
-                archive.add_file("ltrei.bmg", create_letter(srcpath + "/euItalian.json"))
-                archive.add_file("ltres.bmg", create_letter(srcpath + "/euSpanish.json"))
-            elif region == "J" or region == "All":
-                archive.add_file("ltrjj.bmg", create_letter(srcpath + "/jpJapanese.json"))
-            elif region == "K" or region == "All":
-                archive.add_file("ltrkk.bmg", create_letter(srcpath + "/krKorean.json"))
-        if designfile:
-            archive.add_file("design.bin", read_file("designs/" + designfile))
-        if npcfile:
-            print("NPCs are not supported yet.")
+                archive.add_file("ltrue.bmg", create_letter(dlc_info, "UsEnglish", itemnames))
+                archive.add_file("ltruf.bmg", create_letter(dlc_info, "UsFrench", itemnames))
+                archive.add_file("ltrus.bmg", create_letter(dlc_info, "UsSpanish", itemnames))
+            if region == "P" or region == "All":
+                archive.add_file("ltree.bmg", create_letter(dlc_info, "EuEnglish", itemnames))
+                archive.add_file("ltref.bmg", create_letter(dlc_info, "EuFrench", itemnames))
+                archive.add_file("ltreg.bmg", create_letter(dlc_info, "German", itemnames))
+                archive.add_file("ltrei.bmg", create_letter(dlc_info, "Italian", itemnames))
+                archive.add_file("ltres.bmg", create_letter(dlc_info, "EuSpanish", itemnames))
+            if region == "J" or region == "All":
+                archive.add_file("ltrjj.bmg", create_letter(dlc_info, "Japanese", itemnames))
+            if region == "K" or region == "All":
+                archive.add_file("ltrkk.bmg", create_letter(dlc_info, "Korean", itemnames))
+        if design_data:
+            archive.add_file("design.bin", design_data)
+        if npc_data:
+            archive.add_file(npc_file_name, npc_data)
 
-        outbin = archive.save()
-        outpath = "build/" + dlcname + region + ".arc"
+        # Save and encrypt the archive if possible
+        output = archive.save()
+        out_path = "build/" + dlc_name + "_" + region + ".arc"
 
         if is_wc24_keys_available():
             if keep_decrypted:
-                write_file(outpath, outbin)
-            outpath += ".wc24"
-            outbin = encrypt(outbin)
+                write_file(out_path, output)
+            out_path += ".wc24"
+            output = encrypt(output)
         else:
-            print("Skipped RSA-AES signing due to missing key file.")
+            print("Skipped RSA-AES signing due to missing key(s).")
 
-        write_file(outpath, outbin)
+        write_file(out_path, output)
 
 
 def extract(dlcname: str):
